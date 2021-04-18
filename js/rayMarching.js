7f384806ea2f964468fe6dabef08cd8ef79b3ca7
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-04-18 20:54:49 +0200

    add the rayMarching

--- js/rayMarching.js
+++ js/rayMarching.js
@@ -1,28 +1,27 @@
-var points = []
-var qtree;
+var obstacles = []
+var obstaclesNb = 20
 var player;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    var boundaries = new Rectangle(0, 0, width, height)
-    qtree = new Quadtree(boundaries, 4)
+    for (let i = 0; i < obstaclesNb; i++) {
+        obstacles.push(new Obstacles())
+    }
+    player = new Player()
 }
 
 function draw() {
-    background(255)
-    for (let i = 0; i < points.length; i++) {
-        points[i].render()
+    background(40)
+    for (let i = 0; i < obstacles.length; i++) {
+        obstacles[i].render()
     }
-    if (mouseIsPressed) {
-        points.push(new Point(mouseX, mouseY, 4))
-        qtree.insert(points[points.length - 1])
-    }
-    qtree.show()
+    player.update()
 }
 
 class Player {
     constructor() {
-        this.position = createVector(random(width), random(height))
+        this.position = createVector(mouseX, mouseY)
+        this.radius = 20
         this.ray = p5.Vector.random2D()
         this.rotateAngle = 0.004
     }
@@ -31,28 +30,74 @@ class Player {
         this.drawRay()
         this.ray.x = Math.cos(this.rotateAngle) * this.ray.x - Math.sin(this.rotateAngle) * this.ray.y
         this.ray.y = Math.sin(this.rotateAngle) * this.ray.x + Math.cos(this.rotateAngle) * this.ray.y
+        this.position = createVector(mouseX, mouseY)
+        this.pointChild = new Point(this.position.x, this.position.y, this.ray)
     }
     show() {
-        strokeWeight(10)
-        stroke(150)
+        strokeWeight(this.radius)
+        stroke(255)
         point(this.position.x, this.position.y)
     }
     drawRay() {
-        strokeWeight(3)
+        stroke(200)
+        strokeWeight(1)
         var len = createVector(windowWidth, windowHeight).mag()
         this.ray.setMag(len)
         line(this.position.x, this.position.y, this.position.x + this.ray.x, this.position.y + this.ray.y)
     }
 }
 
-class Obstacle {
+class Point {
+    constructor(x, y, ray) {
+        this.position = createVector(x, y)
+        this.radius = 4
+        this.ray = ray
+        this.show()
+        this.rayCast()
+    }
+    show() {
+        strokeWeight(this.radius)
+        stroke(200)
+        point(this.position.x, this.position.y)
+    }
+    rayCast() {
+        var diag = createVector(windowWidth, windowHeight).mag()
+        var minDist = diag
+        for (let obstacle of obstacles) {
+            var distance = dist(this.position.x, this.position.y, obstacle.position.x, obstacle.position.y) - obstacle.radius / 2;
+            if (distance <= this.radius / 2) {
+                minDist = 0
+            } else if (distance < minDist) {
+                minDist = distance
+            }
+        }
+        stroke(200)
+        strokeWeight(1)
+        fill('rgba(200,200,200, 0.10)')
+        circle(this.position.x, this.position.y, minDist * 2)
+        if (minDist < diag) {
+            if (minDist >= 0.01) {
+                this.ray.setMag(minDist)
+                var newPos = createVector(this.position.x + this.ray.x, this.position.y + this.ray.y)
+                console.log(newPos)
+                new Point(newPos.x, newPos.y, this.ray)
+            } else {
+                stroke(200, 0, 0)
+                strokeWeight(10)
+                point(this.position.x, this.position.y)
+            }
+        }
+    }
+}
+
+class Obstacles {
     constructor() {
         this.position = createVector(random(width), random(height))
         this.radius = random(20, 100)
     }
-    show() {
-        strokeWeight(this.radius)
-        stroke(100)
-        point(this.position.x, this.position.y)
+    render() {
+        strokeWeight(0)
+        fill(0)
+        circle(this.position.x, this.position.y, this.radius)
     }
 }
