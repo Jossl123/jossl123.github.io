d378bcf184deb5219fd91bce28165568f9703f32
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-04-22 13:39:56 +0200

    finish collatz conjecture

--- js/collatzConjecture.js
+++ js/collatzConjecture.js
@@ -1,40 +1,54 @@
 var origin
-var rotateAngle = 10
-var numbers = 10
+var rotateAngle = Math.PI / 20
+var numbers = 10000
+var lineLength = 10
+var allResults = []
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    origin = createVector(windowWidth / 2, windowHeight)
+    origin = createVector(windowWidth / 2, windowHeight / 2)
     background(0)
     for (let nb = 1; nb <= numbers; nb++) {
         var currentNb = nb
+        var results = [currentNb]
+        while (currentNb != 1) {
+            currentNb = conjecture(currentNb)
+            results.push(currentNb)
+        }
+        results.reverse()
+        allResults.push(results)
+    }
+}
+var itt = 0
+
+function draw() {
+    if (itt < allResults.length) {
+        var results = allResults[itt]
         var dir = createVector(0, -1)
         var pos = createVector(origin.x, origin.y)
         var prevPos = pos
-        var iter = 0
-        while (conjecture(currentNb) != 1) {
-            if (currentNb % 2 == 0) {
+        for (let j = 0; j < results.length; j++) {
+            rotateAngle = Math.PI / random(10, 30)
+            if (results[j] % 2 == 0) {
                 dir.x = Math.cos(rotateAngle) * dir.x - Math.sin(rotateAngle) * dir.y
                 dir.y = Math.sin(rotateAngle) * dir.x + Math.cos(rotateAngle) * dir.y
-                dir.setMag(10)
+                dir.setMag(lineLength)
                 pos.add(dir)
             } else {
                 dir.x = Math.cos(-rotateAngle) * dir.x - Math.sin(-rotateAngle) * dir.y
                 dir.y = Math.sin(-rotateAngle) * dir.x + Math.cos(-rotateAngle) * dir.y
-                dir.setMag(10)
+                dir.setMag(lineLength)
                 pos.add(dir)
             }
-            fill(1 / numbers * nb * 255)
-            stroke(1 / numbers * nb * 255)
+            fill((results.length - j + 1) * 1 / results.length * 255)
+            stroke((results.length - j + 1) * 1 / results.length * 255)
             strokeWeight(1)
             line(pos.x, pos.y, prevPos.x, prevPos.y)
-            currentNb = conjecture(currentNb)
             prevPos = createVector(pos.x, pos.y)
         }
+        itt++
     }
 }
-
-function draw() {}
 
 function conjecture(nb) {
     if (nb % 2 == 0) {
