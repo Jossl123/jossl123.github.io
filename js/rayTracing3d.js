415a0761d44b6451358d101283752af1f7df612c
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-06-04 16:58:14 +0200

    recode all the raytracing

--- js/rayTracing3d.js
+++ js/rayTracing3d.js
@@ -1,10 +1,15 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
-var rez = 20
+var minRez = 20
+var rez = minRez
 var cam
 var scene
 var light
 var lighting = []
+var distMax = 1000
+var maxStep = 40
+var touchDist = 0.1
+var alreadyCalculted = false
 
 function setup() {
     createCanvas(w, h)
@@ -13,7 +18,7 @@ function setup() {
         new Sphere(-30, 5, 20, 10, [parseInt(random() * 250), parseInt(random(250)), 100]),
         new Sphere(10, 1, 10, 5, [parseInt(random(250)), parseInt(random(250)), 100]),
         new Sphere(2, 2, 2, 2, [parseInt(random(250)), parseInt(random(250)), 100]),
-        new Sphere(0, -1000, 100, 1000, [parseInt(random(250)), parseInt(random(250)), 100])
+        new Sphere(0, -100, 0, 100, [parseInt(random(250)), parseInt(random(250)), 100])
     ]
     light = new Light()
     lighting.push(light)
@@ -24,16 +29,29 @@ function draw() {
         loadPixels();
         for (let x = 0; x < w; x += rez) {
             for (let y = 0; y < h; y += rez) {
-                var ax = parseInt(-cam.fov / 2) + (cam.fov * x / w);
-                var ay = parseInt(-(h * cam.fov / w) / 2) + ((h * cam.fov / w) * y / h)
-                var color = castRay(cam.pos, rotateVectorX(rotateVectorY(cam.dir, ax), ay))
-                for (let i = 0; i < rez; i++) {
-                    for (let j = 0; j < rez; j++) {
-                        var index = ((x + i) + (y + j) * w) * 4
-                        pixels[index] = color[0]
-                        pixels[index + 1] = color[1]
-                        pixels[index + 2] = color[2]
-                        pixels[index + 3] = 255;
+                for (let i = 0; i < minRez - rez; i++) {
+                    if ((x + y * w) * 4 / (minRez - i) == minRez - i) {
+                        alreadyCalculted = true;
+                        break;
+                    } else {
+                        alreadyCalculted = false;
+                    }
+                }
+                if (!alreadyCalculted) {
+                    var newDir = createVector(x - w / 2, h / 2 - y, 0);
+                    newDir = rotateVectorX(rotateVectorY(newDir, cam.ay), cam.ax)
+                    newDir = cam.dir.copy().add(newDir)
+                    var color = castRay(cam.pos, newDir)
+                    for (let i = 0; i < rez; i++) {
+                        for (let j = 0; j < rez; j++) {
+                            if (x + i < w && y + j < h) {
+                                var index = ((x + i) + (y + j) * w) * 4
+                                pixels[index] = color[0]
+                                pixels[index + 1] = color[1]
+                                pixels[index + 2] = color[2]
+                                pixels[index + 3] = 255;
+                            }
+                        }
                     }
                 }
             }
@@ -43,69 +61,74 @@ function draw() {
     }
 }
 
-class Camera {
-    constructor() {
-        this.pos = createVector(0, 2, -10);
-        this.fov = 90;
-        this.dir = createVector(0, 0, 1);
-    }
-}
-
-class Light {
-    constructor() {
-        this.pos = createVector(10, 10, -10);
-        this.r = 2;
-        this.color = [250, 240, 240]
-    }
-}
-
-class Sphere {
-    constructor(x, y, z, r, color) {
-        this.pos = createVector(x, y, z);
-        this.r = r
-        this.color = color
-    }
-}
-
+//cast the ray (call for every pixels)
 function castRay(origin, dir) {
-    var distmax = 1000;
-    var minDist = distmax
+    var rayResult = rayMarch(origin, dir)
+        //return a color
+    if (rayResult.objTouch) {
+        return getLight(rayResult)
+    } else {
+        return [rayResult.totalDist, rayResult.totalDist, rayResult.totalDist]
+    }
+}
+
+//ray march and return the distance cross and if touch obj return it and it's touch point
+function rayMarch(origin, dir) {
     var totalDist = 0
+    var point = origin.copy()
     var objTouch
-    var currentPoint = origin.copy()
-    while (totalDist < distmax && minDist > 1) {
-        minDist = distmax
-        for (let obj of scene) {
-            var dist = distancePoints(obj.pos, currentPoint) - obj.r
-            if (dist < minDist) {
-                minDist = dist
+    for (let step = 0; step < maxStep; step++) {
+        var distobj = getDistToAllObj(point)
+        var dist = distobj[0]
+        objTouch = distobj[1]
+        totalDist += dist
+        if (totalDist > distMax || dist < touchDist) break;
+        point.add(dir.setMag(dist))
+    }
+    return { point: point, objTouch: objTouch, dist: totalDist }
+}
+
+//return the shorter distance between all objects
+function getDistToAllObj(point) {
+    var minDist = distMax
+    var objTouch
+    for (let obj of scene) {
+        var dist = distancePoints(obj.pos, point) - obj.r
+        if (dist < minDist) {
+            minDist = dist
+            if (dist <= touchDist) {
                 objTouch = obj
             }
         }
-        currentPoint.add(dir.setMag(minDist))
-        totalDist += minDist
     }
-    if (minDist <= 1) {
-        //if we touch obj
-        return shadow(currentPoint, objTouch)
-    } else {
-        //bg color
-        if (dir.y * dir.z > 0) {
-            return [69, 179, 224]
-        } else {
-            return [0, 120, 54]
-        }
-    }
+    return [minDist, objTouch]
 }
 
-function shadow(p, objTouch) {
-    for (let obj of scene) {
-        var dist = hitSphere(p, light.pos, obj.r, obj.pos)
-        if (dist != -1) {
-            return [objTouch.color[0] * dist, objTouch.color[1] * dist, objTouch.color[2] * dist]
-        }
+//return the normal vector of the shape (the vector which point to is front)
+function getNormal(point) {
+    var dist = getDistToAllObj(point)[0];
+    var n = createVector(
+        dist - getDistToAllObj(point.copy().sub(0.01, 0, 0))[0],
+        dist - getDistToAllObj(point.copy().sub(0, 0.01, 0))[0],
+        dist - getDistToAllObj(point.copy().sub(0, 0, 0.01))[0]
+    )
+    return n.normalize()
+}
+
+//return the color affected by the light environement
+function getLight(rayResult) {
+    var normal = getNormal(rayResult.point)
+    var lightDir = light.pos.copy().sub(rayResult.point).normalize()
+    var diff = normal.dot(lightDir)
+    var rayToLight = rayMarch(rayResult.point.add(lightDir), lightDir)
+    if (rayToLight.dist < distancePoints(rayResult.point, light.pos) && rayToLight.objTouch != rayResult.objTouch) {
+        return mixColor(rayResult.objTouch.color, [0, 0, 0])
     }
-    return objTouch.color
+    return mixColor(rayResult.objTouch.color, [diff * 255, diff * 255, diff * 255])
+}
+
+function mixColor(c1, c2) {
+    return [(c1[0] + c2[0]) / 2, (c1[1] + c2[1]) / 2, (c1[2] + c2[2]) / 2]
 }
 
 function distancePoints(p1, p2) {
@@ -126,76 +149,60 @@ function rotateVectorX(v, a) {
     return createVector(v.x, y, z)
 }
 
-
-function hitSphere(p1, p2, radius, c) { // c = sphere center
-    var A = (p2.x * p1.x + p2.y * p1.y + p2.z * p1.z);
-
-    var B = 2 * ((p2.x - p1.x) * (p1.x - c.x) + (p2.y - p1.y) * (p1.y - c.y) + (p2.z - p1.z) * (p1.z - c.z))
-
-    var C = c.x * c.x + c.y * c.y + c.z * c.z + p1.x * p1.x + p1.y * p1.y + p1.z * p1.z - 2 * (c.x * p1.x + c.y * p1.y + c.z * p1.z) - radius * radius
-
-    var D = B * B - 4 * A * C;
-    var t = -1.0;
-
-    var u = ((c.x - p1.x) * (p2.x - p1.x) + (c.y - p1.y) * (p2.y - p1.y) + (c.z - p1.z) * (p2.z - p1.z)) /
-        ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y) + (p2.z - p1.z) * (p2.z - p1.z))
-
-    if (D >= 0) {
-        var t1 = (-B - Math.sqrt(D)) / (2.0 * A);
-        var t2 = (-B + Math.sqrt(D)) / (2.0 * A);
-        if (t1 <= 1 && t1 >= 0 && t2 < 1) {
-            if (t1 > t2) {
-                t = t1
-            } else {
-                t = t2
-            }
-        } // we choose the nearest t from the first point
-    }
-    return t
-}
-
-// function hitSphere(center, radius, origin, dir) {
-//     // var oc = origin.copy().sub(center);
-//     // var a = dir.dot(dir);
-//     // var b = 2 * oc.dot(dir);
-//     // var c = oc.dot(oc) - radius * radius;
-//     // var discriminant = b * b - 4 * a * c;
-//     // if (discriminant < 0) {
-//     //     return -1;
-//     // } else {
-//     //     var numerator1 = -b - Math.sqrt(discriminant);
-//     //     var numerator2 = -b + Math.sqrt(discriminant);
-//     //     if (numerator1 > 0 && numerator2 > 0) {
-//     //         return Math.abs(((numerator1 / (2 * a)) - (numerator2 / (2 * a)))) * radius;
-//     //     } else {
-//     //         return -1;
-//     //     }
-//     // }
-// }
-
 document.addEventListener("keydown", (e) => {
-    if (e.keyCode == 37) {
-        cam.pos.add(-1, 0, 0)
-        rez = 20
+    if (e.keyCode == 37) { //fleche gauche
+        cam.pos.add(rotateVectorY(cam.dir, -90).setMag(cam.speed))
+        rez = minRez
     }
-    if (e.keyCode == 39) {
-        cam.pos.add(1, 0, 0)
-        rez = 20
+    if (e.keyCode == 39) { //fleche droite
+        cam.pos.add(rotateVectorY(cam.dir, 90).setMag(cam.speed))
+        rez = minRez
     }
-    if (e.keyCode == 40) {
-        cam.pos.add(0, 0, -1)
-        rez = 20
+    if (e.keyCode == 40) { //fleche bas
+        cam.pos.sub(cam.dir.copy().setMag(cam.speed))
+        rez = minRez
     }
-    if (e.keyCode == 38) {
-        cam.pos.add(0, 0, 1)
-        rez = 20
+    if (e.keyCode == 38) { //fleche haut
+        cam.pos.add(cam.dir.copy().setMag(cam.speed));
+        rez = minRez
     }
     if (e.keyCode == 90) { //z key
+        cam.ay += 2
         cam.dir = rotateVectorY(cam.dir, 2)
-        rez = 20
+        rez = minRez
     }
     if (e.keyCode == 65) { //a key
+        cam.ay -= 2
         cam.dir = rotateVectorY(cam.dir, -2)
-        rez = 20
+        rez = minRez
     }
 })
+
+class Camera {
+    constructor() {
+        this.pos = createVector(0, 2, -10);
+        this.fov = 90;
+        this.dir = createVector(0, 0, 1000);
+        this.ax = 0;
+        this.ay = 0;
+        this.az = 0;
+        this.distFromScreen = 1000;
+        this.speed = 2
+    }
+}
+
+class Light {
+    constructor() {
+        this.pos = createVector(10, 10, -10);
+        this.r = 2;
+        this.color = [250, 240, 240]
+    }
+}
+
+class Sphere {
+    constructor(x, y, z, r, color) {
+        this.pos = createVector(x, y, z);
+        this.r = r
+        this.color = color
+    }
+}
