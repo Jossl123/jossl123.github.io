26593d1deaea038d358e95041db143a7d7738720
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2022-04-08 16:10:32 +0200

    make sphere

--- js/3dRenderer.js
+++ js/3dRenderer.js
@@ -2,20 +2,50 @@ var cam
 var points = []
 var cubes = []
 var verticies = []
+var speed = 0.1
+
+function cplane(p, s, r, dir) {
+    if (r <= 1) return
+    var triIndex = points.length
+    var vA = createVector(dir.y, dir.z, dir.x)
+    var vB = vA.cross(dir)
+    for (let y = 0; y < r; y++) {
+        for (let x = 0; x < r; x++) {
+            var percent = createVector(x, y).div(r - 1)
+            var pointOnCube = dir.copy().add(vA.copy().mult((percent.x - 0.5) * 2)).add(vB.copy().mult((percent.y - 0.5) * 2)).mult(s / r)
+            pointOnCube.setMag(s)
+            pointOnCube.add(p)
+            points.push(new Point(pointOnCube.x, pointOnCube.y, pointOnCube.z))
+        }
+    }
+    for (let y = 0; y < r - 1; y++) {
+        for (let x = 0; x < r - 1; x++) {
+            var i = x + y * r
+            verticies.push(new Triangle(points[triIndex + i], points[triIndex + i + 1], points[triIndex + i + r + 1]))
+            verticies.push(new Triangle(points[triIndex + i], points[triIndex + r + i + 1], points[triIndex + i + r]))
+        }
+    }
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     cam = new Camera()
-    cubes.push(new Cube(0, 0, 10, 5, 5, 5))
-    cubes.push(new Cube(5, 0, 10, 5, 5, 5))
+        //cubes.push(new Cube(0, 0, 10, 5, 5, 5))
+        //cubes.push(new Cube(5.1, 0, 10, 5, 5, 5))
         //cubes.push(new Cube(0, 5, 10, 5, 5, 5))
         //cubes.push(new Cube(5, 0, 15, 5, 5, 5))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(0, 1, 0))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(1, 0, 0))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(0, 0, 1))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(0, -1, 0))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(-1, 0, 0))
+    cplane(createVector(0, 1, 5), 2, 2, createVector(0, 0, -1))
     render()
 }
 
 function toScreen(p) {
-    var relativePoint = createVector(p.pos.x, p.pos.y, p.pos.z).sub(cam.pos)
-    if (relativePoint.z >= 1) {
+    var relativePoint = createVector(p.x, p.y, p.z).sub(cam.pos)
+    if (relativePoint.z > 0) {
         var x = relativePoint.x / relativePoint.z * cam.focal_length
         var y = relativePoint.y / relativePoint.z * cam.focal_length
         x *= windowWidth / 2
@@ -36,27 +66,33 @@ function render() {
         var v2 = tri.thirdPoint.pos.copy().sub(tri.firstPoint.pos)
         var crossProduct = v1.cross(v2)
         var toPoint = cam.pos.copy().sub(tri.firstPoint.pos)
-        var centerPoint = tri.firstPoint.pos.copy().add(tri.secondPoint.pos.copy().add(tri.thirdPoint.pos)).div(3)
         var scalaire1 = crossProduct.dot(toPoint)
+        var centerPoint = tri.firstPoint.pos.copy().add(tri.secondPoint.pos.copy().add(tri.thirdPoint.pos)).div(3)
         if (scalaire1 > 0) {
-            var distToCam = centerPoint.mag();
-            var p0 = toScreen(tri.firstPoint)
-            var p1 = toScreen(tri.secondPoint)
-            var p2 = toScreen(tri.thirdPoint)
+            var distToCam = centerPoint.copy().sub(cam.pos).magSq();
+            var p0 = toScreen(tri.firstPoint.pos)
+            var p1 = toScreen(tri.secondPoint.pos)
+            var p2 = toScreen(tri.thirdPoint.pos)
             zOrderIndex.push(distToCam)
             zOrderToDraw.push([p0, p1, p2, distToCam, tri.color])
         }
     }
     zOrderToDraw.sort((a, b) => b[3] - a[3])
-    console.table(zOrderToDraw)
     for (let i = 0; i < zOrderToDraw.length; i++) {
         fill(zOrderToDraw[i][4][0], zOrderToDraw[i][4][1], zOrderToDraw[i][4][2])
+        stroke(zOrderToDraw[i][4][0], zOrderToDraw[i][4][1], zOrderToDraw[i][4][2])
         triangle(zOrderToDraw[i][0][0], zOrderToDraw[i][0][1], zOrderToDraw[i][1][0], zOrderToDraw[i][1][1], zOrderToDraw[i][2][0], zOrderToDraw[i][2][1])
     }
 }
 
 function draw() {
     keyDown()
+}
+
+function VrotateY(v, a) {
+    var x = v.x * Math.cos(a) + v.z * Math.sin(a)
+    var z = -v.x * Math.sin(a) + v.z * Math.cos(a)
+    return createVector(x, v.y, z)
 }
 
 class Camera {
@@ -77,7 +113,7 @@ class Triangle {
         this.firstPoint = firstPoint
         this.secondPoint = secondPoint
         this.thirdPoint = thirdPoint
-        this.color = [random(255), 100, 100]
+        this.color = [100 + random(20), 100, 100]
     }
 }
 
@@ -124,6 +160,7 @@ class Cube {
     }
 }
 
+
 class Light {
     constructor() {
         this.pos = createVector(0, 0, 0)
@@ -133,27 +170,31 @@ class Light {
 function keyDown() {
     var haveToRender = false
     if (keyIsDown(LEFT_ARROW)) {
-        cam.pos.add(-1, 0, 0)
+        cam.pos.add(-speed, 0, 0)
         haveToRender = true
     }
     if (keyIsDown(RIGHT_ARROW)) {
-        cam.pos.add(1, 0, 0)
+        cam.pos.add(speed, 0, 0)
         haveToRender = true
     }
     if (keyIsDown(UP_ARROW)) {
-        cam.pos.add(0, -1, 0)
+        cam.pos.add(0, -speed, 0)
         haveToRender = true
     }
     if (keyIsDown(DOWN_ARROW)) {
-        cam.pos.add(0, 1, 0)
+        cam.pos.add(0, speed, 0)
         haveToRender = true
     }
     if (keyIsDown(90)) {
-        cam.pos.add(0, 0, 1)
+        cam.pos.add(0, 0, speed)
         haveToRender = true
     }
     if (keyIsDown(83)) {
-        cam.pos.add(0, 0, -1)
+        cam.pos.add(0, 0, -speed)
+        haveToRender = true
+    }
+    if (keyIsDown(65)) {
+        cam.dir = VrotateY(cam.dir, 10)
         haveToRender = true
     }
     if (haveToRender) render()
