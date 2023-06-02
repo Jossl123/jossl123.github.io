8cf4468ecced9e63ec9fb1b5ec841ae4f46eaada
Josselin Gautier <josselin.gautier@etu.u-bordeaux.fr>
2023-06-02 10:00:55 +0200

    add springjoint

--- js/retexs.js
+++ js/retexs.js
@@ -1,25 +1,25 @@
-let possibleTags = ["Javascript", "3d", "Java", "SQL", "Python", "C#", "AI", "Bash", "Management"]
+let possibleTags = ["Javascript", "3d", "Java", "SQL", "Python", "C#", "AI", "Bash", "Management", "Physic"]
 let data = {
     "rayTracing3d": {
         "title": "Ray Tracing 3d",
         "description": "    This project is a 3d engine made from scratch (with p5 for the vector classes). <br><br>Raytracing rendering method consist of simulating the inverse of a light ray. In the real life the light comme from the light source to our eyes and in raycsating we launch the ray from the eye to the lights sources.<br><br> We 'launch', for every pixel in the screen, ray (represented by a vector) in a 3d world (represented by some object and their properties like their color). We then found what color did the ray touch (go check out the raymarching project) and color the pixel like so. <br><br> This is just a starting point but if you're insterested go checkout <a href='https://www.youtube.com/watch?v=Qz0KTGYJtUk'>Sebastian Lague videos</a> on the subject : ",
         "method": "- ray tracing <br>\
         - ray marching <br>",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript", "3d"]
     },
     "planetGenerator": {
         "title": "Planet generator",
-        "description": "A planet generator",
+        "description": "Here, you can generate random planets with a specific rezolution and then move around. It was made in the optic of learning how rasterization work. This is a pretty old project but still one of the coolest ones",
         "method": "rasterization",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- math and algorithms knowledge improvements",
         "tags": ["Javascript", "3d"]
     },
     "twotris": {
         "title": "Twotris",
-        "description": "A website to play tetris in multiplayer mode",
+        "description": "A website to play tetris in multiplayer. Thank to <a href='https://github.com/busybox11'>busybox</a> for the front.",
         "method": "Nodejs : websocket and express js",
-        "skills": "javascript skills and websockets",
+        "skills": "- javascript <br>- websockets",
         "link": "https://twotris.onrender.com",
         "tags": ["Javascript"]
     },
@@ -27,8 +27,15 @@ let data = {
         "title": "Ray casting",
         "description": "A raycasting website representing the backrooms",
         "method": "ray casting",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript", "3d"]
+    },
+    "springjoint": {
+        "title": "Spring joint",
+        "description": "spring joint",
+        "method": "physic",
+        "skills": "- Javascript <br>- Math knowledges",
+        "tags": ["Javascript", "Physic"]
     },
     "MLU": {
         "title": "MLU",
@@ -42,42 +49,42 @@ let data = {
         "title": "Transistoshare",
         "description": "A website to simulate some operand gates",
         "method": "reverse polish notation",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "rayMarching": {
         "title": "Raymarching",
         "description": "Ray marching simulation",
         "method": "ray marching",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "mandlebrot2": {
         "title": "Mandlebrot",
         "description": "The mandlebrot fractal",
         "method": "gpu.js",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "boids": {
         "title": "Boids",
         "description": "Birds flight simulation, not very realistic but it is what it is",
         "method": "boids method",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "learnPI": {
         "title": "Learn PI",
         "description": "A website to check your PI knowledge",
         "method": "javascript",
-        "skills": "",
+        "skills": "- Javascript <br>- PI knowledges",
         "tags": ["Javascript"]
     },
     "quadTree": {
         "title": "QuadTree",
         "description": "Quadtree algorithm visualisation implementation",
         "method": "Quadtree",
-        "skills": "Javascript skills and datatypes knowledge improvements",
+        "skills": "- Javascript <br>- QuadTree knowledges",
         "tags": ["Javascript"]
     },
     "beautifulArt": {
@@ -98,14 +105,14 @@ let data = {
         "title": "Collatz conjecture viewer",
         "description": "A simple collatz conjecture visualizer",
         "method": "Collatz conjecture",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "doublePendule": {
         "title": "Double pendular",
         "description": "A double pendular balancing back in force",
         "method": "Double Pendular physic",
-        "skills": "Javascript skills and math knowledge improvements",
+        "skills": "- Javascript <br>- Math knowledges",
         "tags": ["Javascript"]
     },
     "rsk": {
